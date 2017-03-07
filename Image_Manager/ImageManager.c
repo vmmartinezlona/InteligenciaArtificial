c7339a8beac889abc22d47a099d6312d7b88b2cc
Veronica Lona <v.martinezlona@gmail.com>
2017-03-07 11:29:50 -0600

    Changes to be committed: 	new file:   Image_Manager/ImageManager.c
    Image_Manager.c Nueva tarea, apenas en blanco.

---
+++ Image_Manager/ImageManager.c
@@ -0,0 +1,9 @@
+/*
+  INTELIGENCIA ARTIFICIAL
+  SEMESTRE ENERO-JUNIO 2017
+  MARTÍNEZ LONA VERÓNICA MONTSERRAT
+
+  Detección de círculos en imagenes
+*/
+
+//______________________________________Estructuras de datos
